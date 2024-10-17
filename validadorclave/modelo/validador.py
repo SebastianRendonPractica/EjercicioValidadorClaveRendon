f9ba9d8cb8e4c5cb71a27d35a95d19448745bc95
SebastianRendon <srendongrisales@gmail.com>
2024-10-17 12:56:36 -0500

    Cree los 3 metodos restantes

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -10,13 +10,23 @@ class ReglaValidacion(ABC):
         pass
 
     def _validar_longitud(self, clave):
-        return len(clave) > self._longitud_esperada
-    
+        if len(clave) > self._longitud_esperada:
+            return True
+        
+    def _contiene_mayuscula(self, clave):
+        return any(c.isupper() for c in clave)
 
+    def _contiene_minuscula(self, clave):
+        return any(c.islower() for c in clave)
+
+    def _contiene_numero(self, clave):
+        return any(c.isdigit() for c in clave)
 
     
 
 class ReglaValidacionGanimedes:
+
+    def contiene_caracter_especial(self):
     pass
 
 class ReglaValidacionCalisto:
