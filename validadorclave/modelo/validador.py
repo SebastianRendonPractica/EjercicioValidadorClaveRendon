4451413b27f8f0fc9dbaf496a2afda0c8d0a370f
SebastianRendon <srendongrisales@gmail.com>
2024-10-17 12:38:14 -0500

    Agregue dos de las clases de Regla Validacion y su respectivo constructor

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,7 +1,20 @@
 from abc import ABC, abstractmethod
 
 class ReglaValidacion(ABC):
-    pass
+
+    def __init__(self, longitud_esperada):
+        self._longitud_esperada = longitud_esperada
+    
+    @abstractmethod
+    def es_valida(self, clave):
+        pass
+
+    def _validar_longitud(self, clave):
+        return len(clave) > self._longitud_esperada
+    
+
+
+    
 
 class ReglaValidacionGanimedes:
     pass
